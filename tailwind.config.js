7147ac9236a899c96b53c58d65460bf26b6c55bf
apts18o9 <aptevarad90@gmail.com>
2025-09-15 20:26:35 +0530

    updated dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',       // your app folder
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}', // your components folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
